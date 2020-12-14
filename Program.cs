ae0fca3128345496619285c30c7da0ff17c08423
Drew Noakes <git@drewnoakes.com>
2020-12-14 17:48:36 +1100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+var guid = Guid.NewGuid();
+
+if (args.Length == 0)
+{
+    Console.WriteLine(guid);
+    return 0;
+}
+
+if (args.Length == 1)
+{
+    var upper = args[0].ToUpper();
+    switch (upper)
+    {
+        case "N":
+        case "D":
+        case "B":
+        case "P":
+        {
+            var guidString = guid.ToString(upper);
+            if (args[0] == upper)
+                guidString = guidString.ToUpper();
+            Console.WriteLine(guidString);
+            return 0;
+        }
+    }
+}
+
+Console.Error.WriteLine("Usage:");
+Console.Error.WriteLine();
+Console.Error.WriteLine("    guid [option]");
+Console.Error.WriteLine();
+Console.Error.WriteLine("Where [option] is an optional single character that controls formatting:");
+Console.Error.WriteLine();
+Console.Error.WriteLine($"    N    {guid.ToString("N").ToUpper()}");
+Console.Error.WriteLine($"    D    {guid.ToString("D").ToUpper()}");
+Console.Error.WriteLine($"    B    {guid.ToString("B").ToUpper()}");
+Console.Error.WriteLine($"    P    {guid.ToString("P").ToUpper()}");
+Console.Error.WriteLine();
+Console.Error.WriteLine("When unspecified, D formatting is used. Use lowercase [option] for lowercase output.");
+return 1;
---
+++ README.md
@@ -0,0 +1,24 @@
+﻿# guid
+
+Command line tool for generating GUID strings.
+
+## Install
+
+```
+dotnet tool install -g guid
+```
+
+## Usage
+
+```
+guid [option]
+```
+
+Where `[option]` is an optional single character that controls formatting:
+
+- `N` &mdash; `4FDAB48CC00E498CB244D523D5B8156D`
+- `D` &mdash; `4FDAB48C-C00E-498C-B244-D523D5B8156D`
+- `B` &mdash; `{4FDAB48C-C00E-498C-B244-D523D5B8156D}`
+- `P` &mdash; `(4FDAB48C-C00E-498C-B244-D523D5B8156D)`
+
+When unspecified, `D` formatting is used. Use lowercase `[option]` for lowercase output.
